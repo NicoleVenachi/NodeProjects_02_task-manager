8947b987ba95c295c53b4af272ff0b7dc873a945
nicolevenachi <ludwing@unicauca.edu.co>
2023-11-05 18:22:52 -0500

    Server router and controller basic layout

--- routes/tasks.js
+++ routes/tasks.js
@@ -2,13 +2,16 @@
 
 //imports
 const express = require('express');
-const {getAllTasks} = require('../controllers/tasks')
+const {getAllTasks, createTask, getTask, updateTask, deleteTask} = require('../controllers/tasks')
 
 // inicializo router
 const router = express.Router()
 
 router.get('/', getAllTasks) //logica la separo en controllers
 router.post('/', createTask)
+router.get('/:id', getTask)
+router.patch('/:id', updateTask)
+router.delete('/:id', deleteTask)
 
 
 module.exports = router
