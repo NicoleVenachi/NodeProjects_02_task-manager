dd2cd138bcb3bcbf4b98b374aa585f19c8eca38b
nicolevenachi <ludwing@unicauca.edu.co>
2023-11-05 13:58:39 -0500

    server basic setup

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 console.log('Task Manager App')
+
+// imports
+const express = require('express');
+
+// inicializo express app
+const app = express(); 
+
+// routing
+app.get('/hello', (req,res) => {
+
+  res.send('Task Manager App')
+
+})
+// defino el prueto a escuchar
+const PORT = 3000; // || process.env;
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+})
