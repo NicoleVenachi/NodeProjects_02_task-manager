972bbafbff3058d52adabb082358e60e401e3f21
nicolevenachi <ludwing@unicauca.edu.co>
2023-11-06 18:23:39 -0500

    mongoo -R-ead opeation (one instance, i.e., using querying ocndition)

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -34,8 +34,27 @@ const createTask = async (req, res) => {
 
 }
 
-const getTask = (req, res) => {
-  res.json({id: req.params.id}) // test to response using the id on the url params
+const getTask = async (req, res) => {
+
+  const {id: taskId} = req.params //saco el Id de la URL params
+
+  try {
+    // -R-ead method (con querying condition), para 1 único registro
+
+    console.log(taskId);
+    const task = await Model.findOne({_id: taskId});
+    // {configuro la condición, equality}
+
+    console.log(task);
+
+    if (!task) return res.status(404).json({msg: `No task with id: ${taskId}`});
+    // sino encuentra nada/Null, ese id no existe, me lo mandaron mal
+
+    res.status(200).json({task})
+  } catch (error) {
+    res.status(500).json({msg: error})
+  }
+  
 }
 
 const updateTask = (req, res) => {
