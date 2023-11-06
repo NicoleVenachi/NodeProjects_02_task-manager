3d902d77d22b7d2bdb4097a2bf170afeb62efeb7
nicolevenachi <ludwing@unicauca.edu.co>
2023-11-06 17:39:48 -0500

    mongoose built-in validators (constraints)

--- models/Task.js
+++ models/Task.js
@@ -2,8 +2,20 @@
 const mongoose = require('mongoose');
 
 const TaskSchema = new mongoose.Schema({
-  name: String,
-  completed: Boolean
+  name: {
+    type: String, //value
+
+    //--- validators
+    required: [true, 'must provide a name'], //value, con erro/custom message
+    trim: true, //trim de spaces al inico y final
+    maxlength: [20, 'name can not be more than 20 characters'] //value y error message
+  },
+  completed: {
+    type: Boolean,
+
+    default: false, // por defecto false/incompleta
+
+  }
 })
 
 const model = mongoose.model('Task', TaskSchema) //coleccionAEscirbirEnLaDB, SwSchema
